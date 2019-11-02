f53e3569b91a7ab46e37cfc07357407192ad5c46
kokeneko <s17t278@stu.kagawa-u.ac.jp>
2019-11-02 20:51:37 +0900

    set title object

--- src/app/scene/title/controller/TitleController.java
+++ src/app/scene/title/controller/TitleController.java
@@ -2,14 +2,17 @@ package app.scene.title.controller;
 
 import app.scene.Transition;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Polygon;
 
 public class TitleController {
 
-	@FXML Button startButton;
+	@FXML Label titleLogo;
+	@FXML Label startLabel, rankingLabel;
+	@FXML Polygon cursor;
 
 	@FXML
-	private void start() {
+	private void startGame() {
 		Transition transition = new Transition();
 		transition.transitionTo(Transition.GAME_PATH);
 
