148027dc83f607509b386fc2a9e125c8ac2bb0ae
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2019-11-07 17:21:22 +0900

    Make changes to comments #11

--- src/app/scene/game/controller/GameController.java
+++ src/app/scene/game/controller/GameController.java
@@ -3,7 +3,6 @@ package app.scene.game.controller;
 import app.scene.Floor;
 import app.scene.SceneManager;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
@@ -14,17 +13,14 @@ public class GameController {
 	private Button helloButton;
 	@FXML
 	private AnchorPane base;
-
 	private Image image ;
-	public static Group group = new Group();
 
 	@FXML
 	private void initialize() {
 		Floor floor = new Floor();
-		image = floor.assignmentImage("normal");
-		//始めの床を生成する
-		floor.generate(image, 0, 300, 13);
-		base.getChildren().add(group);
+		image = floor.assignImage("normal");
+		//始めの床を生成し、paneに載せる
+		base.getChildren().add(floor.generate(image, 0, 300, 13));
 	}
 	@FXML
 	private void hello() {
