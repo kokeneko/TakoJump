b72ba3669637cb36ba5a83c8aa1ec40d23eeb51c
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2019-11-07 17:21:22 +0900

    改行の削除をしました。
    if文とfor文の空白を除去しました。
    メソッド名をassignImageに変更しました。
    GroupをFloorクラスに変更しました。

--- src/app/scene/Floor.java
+++ src/app/scene/Floor.java
@@ -2,19 +2,19 @@ package app.scene;
 
 import java.net.URISyntaxException;
 
-import app.scene.game.controller.GameController;
+import javafx.scene.Group;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Floor {
 
 	private String type;//ブロックの種類
-
-	public final static String FLOOR_NORMAL = "./images/floor_normal.png" ;
+	public  Group group = new Group(); //床をグループ化する
+	public static final  String FLOOR_NORMAL = "./images/floor_normal.png" ;
 
 	//画像をtype毎に代入する
-	public Image assignmentImage(String type) {
-		if(type.equals("normal")) {
+	public Image assignImage(String type) {
+		if (type.equals("normal")) {
 				try {
 					this.type = getClass().getResource(FLOOR_NORMAL).toURI().toString();
 				} catch (URISyntaxException e) {
@@ -24,16 +24,17 @@ public class Floor {
 		return new Image(this.type);
 	}
 	//床を座標(x, y)にblocks 分生成する
-	public void generate(Image image, double x, double y, int blocks) {
+	public Group generate(Image image, double x, double y, int blocks) {
 		//blocks 分
 		double width = image.getWidth();
-		for(int i = 0;i < blocks;i++) {
+		for (int i = 0; i < blocks; i++) {
 			ImageView imageView = new ImageView();
 			imageView.setImage(image);
 			imageView.setX(x);
 			imageView.setY(y);
-			GameController.group.getChildren().add(imageView);
+			group.getChildren().add(imageView);
 			x += width;
 		}
+		return group;
 	}
 }
