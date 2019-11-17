00ea07e3423c1e0342a7c7ef71f1b9d5123dc5ab
kokeneko <s17t278@stu.kagawa-u.ac.jp>
2019-11-17 19:58:03 +0900

    Improve Layout of score

--- src/app/scene/result/controller/ResultController.java
+++ src/app/scene/result/controller/ResultController.java
@@ -35,14 +35,14 @@ public class ResultController {
 
 		// スコアは仮に適当な値を入れてます
 		// 確認するときは勝手に値を入れてね
-		ResultScoreManager rsm = new ResultScoreManager(scoreLabel, highScoreLabel, newRecordLabel, 5000);
+		ResultScoreManager rsm = new ResultScoreManager(scoreLabel, highScoreLabel, newRecordLabel, 500000);
 
 		FontManager fontManager = new FontManager();
 		gameoverLabel.setFont(fontManager.getFont(50));
 		newRecordLabel.setFont(fontManager.getFont(20));
+		scoreLabel.setFont(fontManager.getFont(40));
 		Font font = fontManager.getFont(25);
 		highScoreTextLabel.setFont(font);
-		scoreLabel.setFont(font);
 		highScoreLabel.setFont(font);
 		restartLabel.setFont(font);
 		toTitleLabel.setFont(font);
